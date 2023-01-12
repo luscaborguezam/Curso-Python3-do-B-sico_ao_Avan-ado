f602e1086c202aee6ef09de2efae26a635342109
Lucas Borguezam <lucasborguezam@gmail.com>
2023-01-12 19:24:15 -0300

    1° commit

---
+++ Aulas/sesao2/teste.py
@@ -0,0 +1,5 @@
+# Em uma linguagem de tipagem dinâmica e forte, o próprio interpretador determina o tipo de um valor quando criado.
+# É possível modificar um valor interpretado de um tipo para outro realizando um "Type Cast" (ou cast) em alguns casos,
+# mas o interpretador não fará isso por você.
+
+x = input()
